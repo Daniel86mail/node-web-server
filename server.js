5f9535411c2ad2d48bbf2a48d896a82ea97673e2
Daniel Ciuraru <daniel86mail@gmail.com>
2018-05-21 14:57:40 +0300

    templating with HBS

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname+ '/public'));
+
 app.get('/', (req, res)=>{  //incoming req (variable etc), res - the answer
     //res.send('<h1>Hello Express</h1>');
-    res.send({
-        name: 'Daniel',
-        likes: [
-            'Suki',
-            'Running'
-        ]
-    });
+    // res.send({
+    //     name: 'Daniel',
+    //     likes: [
+    //         'Suki',
+    //         'Running'
+    //     ]
+    // });
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Hello, welcome!!'
+    })
 });
 
 app.get('/about', (req, res)=>{
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req, res)=>{
